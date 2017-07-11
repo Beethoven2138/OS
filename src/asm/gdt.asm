54e680c14ffd49fe166f37d88c73d8834c76160d
Saxon <saxon.supple@gmail.com>
2017-07-11 15:49:05 +0100

    Update gdt.asm

--- src/asm/gdt.asm
+++ src/asm/gdt.asm
@@ -1,11 +1,8 @@
-; This will set up our new segment registers. We need to do
-; something special in order to set CS. We do what is called a
-; far jump. A jump that includes a segment as well as an offset.
-; This is declared in C as 'extern void gdt_flush();'
-global gdt_flush     ; Allows the C code to link to this
-extern gp            ; Says that '_gp' is in another file
+
+global gdt_flush
+extern gp
 gdt_flush:
-    lgdt [gp]        ; Load the GDT with our '_gp' which is a special pointer
+    lgdt [gp]
     mov ax, 0x10      ; 0x10 is the offset in the GDT to our data segment
     mov ds, ax
     mov es, ax
